27ec74b2b9278528f31fb46fefa059c8f5eae1b3
이창민 책임 <hwisaek@hyundai-autoever.com>
2022-09-21 16:20:41 +0900

    ch1 - problem1

---
+++ ch1/1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		log.Panic("need one or more arguments")
+	}
+
+	sum := 0.
+	for i := 1; i < len(args); i++ {
+		float, err := strconv.ParseFloat(args[i], 1218)
+		if err == nil {
+			sum += float
+		}
+	}
+	fmt.Println(sum)
+}
